15c32358e0bd04c45eec2a8d215ed3815a81dcfc
thienkimtm211 <kimtruong0211@gmail.com>
2019-08-01 19:01:40 +0700

    cap nhat Tao bang

--- loaisanpham/danhsach.php
+++ loaisanpham/danhsach.php
@@ -12,5 +12,19 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         'lsp_mota' => $row['lsp_mota'],
     );
 }
-print_r($data);die;
+//print_r($data);die;
 ?>
+<table border="1">
+    <tr>
+        <th>Mã</th>
+        <th>Tên</th>
+        <th>Mô tả</th>
+    </tr>
+    <?php foreach($data as $row) : ?>
+    <tr>
+        <td><?php echo $row['lsp_ma']; ?></td>
+        <td><?= $row['lsp_ten']; ?></td>
+        <td><?= $row['lsp_mota']; ?></td>
+    </tr>
+<?php endforeach; ?>
+</table>
