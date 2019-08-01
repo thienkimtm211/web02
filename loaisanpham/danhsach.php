5b235e19c4e2c3e13aaa83ad72188634bbeb723e
thienkimtm211 <kimtruong0211@gmail.com>
2019-08-01 19:27:03 +0700

    Cap nhat chuc nang Sua

--- loaisanpham/danhsach.php
+++ loaisanpham/danhsach.php
@@ -14,17 +14,29 @@ while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 }
 //print_r($data);die;
 ?>
+<style>
+.rowchan {
+    background: red;
+}
+</style>
 <table border="1">
     <tr>
         <th>Mã</th>
         <th>Tên</th>
         <th>Mô tả</th>
+        <th>Chức năng</th>
     </tr>
+    <?php 
+        $bienDem = 1;
+    ?>
     <?php foreach($data as $row) : ?>
-    <tr>
+    <tr class="<?php echo ($bienDem % 2 == 0 ? 'rowchan' : '')?>">
         <td><?php echo $row['lsp_ma']; ?></td>
         <td><?= $row['lsp_ten']; ?></td>
         <td><?= $row['lsp_mota']; ?></td>
+        <!-- Truyền dữ liệu GET trên URL, theo dạng ?KEY1=VALUE1&KEY2=VALUE2 -->
+        <td><a href="/web02/loaisanpham/sua.php?khoachinh=<?= $row['lsp_ma'];?>&khoabimat=1234">Sửa</a></td>
     </tr>
+<?php $bienDem++ ;?>
 <?php endforeach; ?>
 </table>
---
+++ loaisanpham/sua.php
@@ -0,0 +1,7 @@
+<h1>Đây là chức năng sửa loại sản phẩm</h1>
+<?php
+    $lsp_ma = $_GET['khoachinh'];
+    $khoabimat = $_GET['khoabimat'];
+    echo 'Đang sửa khóa chính là:' . $lsp_ma . '<br />';
+    echo 'Đang sửa khóa bí mật là: ' . $khoabimat;
+?>
